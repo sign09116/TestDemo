662d1f1d26810eb5e533edbb91aa70965eb4be32
sign09116 <57554727+sign09116@users.noreply.github.com>
2020-01-04 18:03:10 +0800

    將輸入欄位的文字轉為浮點數顯示

---
+++ Test/Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Text inputField, Show;
+    float t=0;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+            float.TryParse(inputField.text,out t);
+            print(t);
+            Show.text = t.ToString();
+        }
+    }
+    
+}
